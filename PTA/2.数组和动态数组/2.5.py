f45742c5f3bafb4db0fba7b7465486397d6ded35
HanYouyang <hanyouyang93@gmail.com>
2019-10-31 11:36:50 +0800

    上传自己确定的模式

---
+++ PTA/2.数组和动态数组/2.5.py
@@ -0,0 +1,28 @@
+# 模拟
+import random
+def minesweeper(m, n, p):
+    board = [[None] * (n + 2) for i in range(m + 2)]
+    # range是取左不取右的
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            r = random.random()
+            board[i][j] = -1 if r < p else 0
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            print('*', end = ' ') if board[i][j] == -1 else print('.', end = ' ')
+        print()# 打印空行
+        
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if (board[i][j] != -1):
+                for ii in range(i - 1, i + 2):
+                    for jj in range(j - 1, j + 2):
+                        if (borad[ii][jj] == -1):
+                            borad[i][j] += 1
+    
+    print()
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            print('*', end = ' ') if board[i][j] == -1 else print(borad[i][j], end = ' ')
+        print()
